a1ca8520db623749316d0cf38f53b28e826d16e5
quockhang <quockhangtr@gmail.com>
2019-10-18 17:53:30 -0500

    fixed bug in linked list transversal in getremainingmemory and show list

--- UtPod.cpp
+++ UtPod.cpp
@@ -3,25 +3,42 @@
 #include "Song.h"
 #include "UtPod.h"
 
-
+using namespace std;
 
 UtPod::UtPod(){
-
+    songs = NULL;
+    memSize = MAX_MEMORY;
     }
 
 
 UtPod::UtPod(int size){
-
+    songs = NULL;
+    if(size > MAX_MEMORY || size <= 0){
+        memSize = MAX_MEMORY;
+    }
+    else{
+        memSize = size;
+    }
 }
 
 
 int UtPod::addSong(Song const &s){
+
+        SongNode *temp = new SongNode;
+        temp->next = songs;
+        songs = temp;
+        temp->s.setTitle(s.getTitle());
+        temp->s.setArtist(s.getArtist());
+        temp->s.setSize(s.getSize()); 
+        return 0;
+    
 
 }
 
 
 
 int UtPod::removeSong(Song const &s){
+    return 0;
 
 }
 
@@ -35,7 +52,16 @@ void UtPod::shuffle(){
 
 
 void UtPod::showSongList(){
-
+    if(songs == NULL){
+        cout<<"list is empty"<<endl;
+    }
+    else{
+        SongNode *ptr = songs;
+        while(ptr != NULL){
+            cout << ptr->s.getTitle()<< " by " << ptr->s.getArtist() << " size of: " << ptr->s.getSize() << endl;
+            ptr = ptr->next;
+        }
+    }
 }
 
 
@@ -52,10 +78,21 @@ void UtPod::clearMemory(){
 
 
 int UtPod::getRemainingMemory(){
-
+    int curSize = 0;
+    if(songs == NULL){
+        return memSize;
+    }
+    else{
+        SongNode *ptr = songs;
+        while(ptr != NULL){
+            curSize += ptr->s.getSize();
+            ptr = ptr->next;
+        }
+    }
+    return memSize - curSize;
 }
 
 
 UtPod::~UtPod(){
-    
+
 }
